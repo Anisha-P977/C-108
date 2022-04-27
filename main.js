c1f97dbc5e681f11759812f1df46ce3c42657805
Anisha <74487792+Anisha-P977@users.noreply.github.com>
2022-04-27 12:05:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,57 +1,53 @@
-function startClassification() {
-    navigator.mediaDevices.getUserMedia({ audio: true });
+function startClassification()
+{
+  navigator.mediaDevices.getUserMedia({ audio: true});
     classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/h3cc2MxR7/model.json', modelReady);
-
 }
 
-function modelReady() {
-    classifier.classify(gotResults);
+function modelReady(){
+  classifier.classify( gotResults);
 }
-
 
 function gotResults(error, results) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(results);
-        random_number_r = Math.floor(Math.random() * 255) + 1;
-        random_number_g = Math.floor(Math.random() * 255) + 1;
-        random_number_b = Math.floor(Math.random() * 255) + 1;
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+//get decimal random number between 0 to 1 using Math.random()
+    random_number_r = Math.floor(Math.random() * 255) + 1; //0.2 * 255 = 51.56 //51+1= 52
+    random_number_g = Math.floor(Math.random() * 255) + 1; //0 * 255 = 0 + 1 = 1
+    random_number_b = Math.floor(Math.random() * 255) + 1;
 
-        document.getElementById("result_label").innerHTML = 'I can hear - ' +
-            results[0].label;
-        document.getElementById("result_confidence").innerHTML = 'Accuracy - ' +
-            (results[0].confidence * 100).toFixed(2) + " %";
-        document.getElementById("result_label").style.color = "rgb("
-            + random_number_r + "," + random_number_g + "," + random_number_b + ")";
-        document.getElementById("result_confidence").style.color = "rgb("
-            + random_number_r + "," + random_number_g + "," + random_number_b + ")";
+    document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label; //clapping
+    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %"; // 0.56 * 100 = 56.00%
+    document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")"; // rgb(200,130,48)
+    document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
 
-        img = document.getElementById('alien1');
-        img1 = document.getElementById('alien2');
-        img2 = document.getElementById('alien3');
-        img3 = document.getElementById('alien4');
+    img = document.getElementById('alien1') 
+    img1 = document.getElementById('alien2')
+    img2 = document.getElementById('alien3')
+    img3 = document.getElementById('alien4')
 
-        if (results[0].label == "Clap") {
-            img.src = 'aliens-01.gif';
-            img1.src = 'aliens-02.png';
-            img2.src = 'aliens-03.png';
-            img3.src = 'aliens-04.png';
-        } else if (results[0].label == "Bell") {
-            img.src = 'aliens-01.png';
-            img1.src = 'aliens-02.gif';
-            img2.src = 'aliens-03.png';
-            img3.src = 'aliens-04.png';
-        } else if (results[0].label == "Snap") {
-            img.src = 'aliens-01.png';
-            img1.src = 'aliens-02.png';
-            img2.src = 'aliens-03.gif';
-            img3.src = 'aliens-04.png';
-        } else {
-            img.src = 'aliens-01.png';
-            img1.src = 'aliens-02.png';
-            img2.src = 'aliens-03.png';
-            img3.src = 'aliens-04.gif';
-        }
+    if (results[0].label == "Clap") {
+      img.src = 'aliens-01.gif';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.png';
+    } else if (results[0].label == "Bell") {
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.gif';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.png';
+    } else if (results[0].label == "Snapping") {
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.gif';
+      img3.src = 'aliens-04.png';
+    }else{
+      img.src = 'aliens-01.png';
+      img1.src = 'aliens-02.png';
+      img2.src = 'aliens-03.png';
+      img3.src = 'aliens-04.gif';
     }
+  }
 }
